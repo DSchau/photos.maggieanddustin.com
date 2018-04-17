e20032bfdd33ddedd6e8a38ded0f142ad20461de
Dustin Schau <DSchau@users.noreply.github.com>
2018-04-16 19:31:28 -0500

    feat: add location support (#1)
    * feat: add location support
    
    * chore: do not limit ya dingus

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,40 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path');
+const slugify = require('limax');
 
- // You can delete this file if you're not using it
+exports.createPages = function createPages({ boundActionCreators, graphql }) {
+  const { createPage } = boundActionCreators;
+
+  return graphql(`
+    {
+      images:allS3Image(
+          sort:{order:DESC, fields: [LastModified]}
+        ) {
+          edges {
+            node {
+              Key
+            }
+          }
+        }
+    }
+  `)
+    .then(result => {
+      if (result.errors) {
+        return Promise.reject(result.errors);
+      }
+
+      const { images } = result.data;
+
+      const locationTemplate = path.resolve('src/templates/location.js');
+
+      images.edges.forEach(({ node }) => {
+        const location = slugify(node.Key.split('/').slice(0, 1).pop().replace(/^[\d-]+/, ''));
+        createPage({
+          path: `/location/${location}`,
+          component: locationTemplate,
+          context: {
+            location: `/${location}.+/`
+          }
+        });
+      });
+    })
+};
